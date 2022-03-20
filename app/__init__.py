cbfe1ad4ae54b1b6809f652c2e77ac38652b243f
Thibault Fernandez <thibault.fernandez81@gmail.com>
2022-03-20 13:55:01 +0100

    Mise en place du style de la page d'accueil de l'application. Vérification du bon fonctionnement de l'API avec différentes variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template
-from pprint import pprint
-
-crypto1 = "Bitcoin"
-crypto2 = "Ethereum"
+# from pprint import pprint
 
 
+total_invests = 5000
 
 def create_app():
     app = Flask(__name__)
-
-    
 
     @app.route('/')
     def homepage():
         from app.api import get_crypto
         data = get_crypto()
+        
+        btc_symbol = list(data['data'])[0]
+        btc_name = data['data']['BTC']['name']
+        eth_symbol = list(data['data'])[1]
+        eth_name = data['data']['ETH']['name']
 
-        return render_template('index.html', crypto1 = data['data']['BTC']['name'], crypto2 = data['data']['ETH']['name'])
+        return render_template('index.html', btc_name = btc_name,
+                               btc_symbol = btc_symbol,
+                               eth_symbol = eth_symbol,
+                               eth_name = eth_name,
+                               total_invests = total_invests)
 
     @app.route('/about/')
     def about():
         return 'This is the about page'
-
-    
 
     return app
