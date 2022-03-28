6ed2a01d9cd6f96da79e33ef0d00fa5f4fb64de2
Thibault Fernandez <thibault.fernandez81@gmail.com>
2022-03-28 14:15:33 +0200

    Ajout de la fonctionnalité permettant d'acheter des cryptos et de modifier la quantité achetée dans la base de données

--- app/__init__.py
+++ app/__init__.py
@@ -10,21 +10,31 @@ with open("app/list_crypto.json", "r") as f:
 # def create_app():
 app = Flask(__name__)
 
+# initialisation de la page d'accueil de l'application
 @app.route('/')
 def homepage():
 
     return render_template('index.html', crypto_list=crypto_list)
 
+
+
 @app.route('/achat/', methods=["POST", "GET"])
 def buyPage():
     
+    # On récupère la base de données json que l'on instancie avec la variable "crypto_list"
+    with open("app/list_crypto.json", "r") as f:
+        crypto_list = json.load(f)
+    
+    # On récupère la crypto choisie avec la quantité voulu, et on ajoute cette quantité à la crypto en question
+    crypto = request.form['select_crypto']
+    quantity = request.form['quantity_crypto']
+    crypto_list[crypto]['quantity'] = float(quantity)
+    
+    # On écrase la base de données avec la quantité que l'on a ajouté à la crypto choisie
+    with open("app/list_crypto.json", "w") as f:
+        json.dump(crypto_list, f, indent=4)
+    
     return render_template('achat.html', crypto_list=crypto_list)
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
